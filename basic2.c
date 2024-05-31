9953cb99cc8e3295b413918560ddd70bf3f08db5
Anushkothuri <kothurianush16@gmail.com>
2024-05-31 12:26:33 +0530

    read

---
+++ basic2.c
@@ -0,0 +1,11 @@
+// To read string and print.
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    printf("enter a string:");
+    scanf("%s",&str);
+    printf("string is %s",str);
+    return 0;
+}
