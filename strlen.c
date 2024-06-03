15c5c6c90cc620f978edac6986c50a5530ed14bc
Anushkothuri <kothurianush16@gmail.com>
2024-06-03 11:44:26 +0530

    strlen

---
+++ strlen.c
@@ -0,0 +1,12 @@
+//Understanding how to find length of a string.
+// Using strlen() function.
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[20]="Hello Macha";
+    int result;
+    result=strlen(str1);
+    printf("length of the string:%d",result); // Output is 11.
+    return 0;
+}
