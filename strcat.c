1c9a95227feb5e7de9e8694840322c4c38163edf
Anushkothuri <kothurianush16@gmail.com>
2024-06-03 12:24:04 +0530

    strcat

--- strcat.c
+++ strcat.c
@@ -1,12 +1,12 @@
-// Understanding strcat function.
+// Understanding how to concatinate two strings.
+// Using strcat function.
 #include <stdio.h>
 #include <string.h>
 int main() 
 {
-    char str1[20] = "Hello World! ";
-    char str2[20] = "Welcome to C.";
+    char str1[]="Hello";
+    char str2[]=" Macha";
     strcat(str1,str2);
-    str2[sizeof(str2) - 1] = '\0'; // Ensure null-termination
-    printf("%s",str1);
+    printf("Concatinated string is : %s",str1);
     return 0;
 }
