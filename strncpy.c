4dba3fdb100d9a23cc15609af3d9bc758ad172c4
Anushkothuri <kothurianush16@gmail.com>
2024-06-03 12:16:51 +0530

    strncpy

--- strncpy.c
+++ strncpy.c
@@ -1,13 +1,16 @@
-// Understanding strncpy function.
+// Understanding how to copy string from one string to another explicitly.
+// Using strncpy() function.
 #include <stdio.h>
 #include <string.h>
 int main() 
 {
-    char source[] = "Hello, World!";
+    char source[]="Hello";
     char destination[20];
-    strncpy(destination, source, sizeof(destination) - 1);
-    destination[sizeof(destination) - 1] = '\0'; // Ensure null-termination
-    printf("Source: %s\n", source);
-    printf("Destination: %s\n", destination);
+    // specified how many characters will be copied to destination string.
+    strncpy(destination,source,3);
+    // explicitly ensuring NULL terminator.
+    destination[3]='\0';
+    printf("source string : %s\n",source);
+    printf("Destination string: %s",destination);
     return 0;
 }
