d926fbed7e40738a34efe2a0c0d4ca528d6bba7d
Anushkothuri <kothurianush16@gmail.com>
2024-06-13 11:26:53 +0530

    strncat

--- strncat.c
+++ strncat.c
@@ -4,8 +4,8 @@
 #include<string.h>
 int main()
 {
-    char str1[]="Hello";
-    char str2[]=" Macha";
+    char str1[20]="Hello";
+    char str2[]=" World!";
     strncat(str1,str2,3);
     printf("Concatination string: %s",str1);
     return 0;
